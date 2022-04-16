c80af58ed48456fb0292139d2429d8c8c8d6ee58
CHOW Hau Cheung Jasper <hcjchow@connect.ust.hk>
2022-04-17 00:53:04 +0800

    remove requirements_ppo.txt

--- test_ppo.py
+++ test_ppo.py
@@ -1,5 +1,9 @@
 from big2_rl.evaluation.ppo_agent import *
+#from torchsummary import summary
+#from big2_rl.deep_mc.model import *
 
 if __name__ == '__main__':
     ppo_agent = PPOAgent()
+    #big2m = Big2Model()
+    #summary(big2m, [(4, 208), (559, )])
     print("success")
